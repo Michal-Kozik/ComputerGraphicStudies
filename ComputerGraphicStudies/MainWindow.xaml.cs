c1c7026ac11c0c0e7e733567e43373a12343cc44
Michal Kozik <michal.kozikowski99@gmail.com>
2021-10-11 22:26:13 +0200

    Zad1- Zrobione przeciąganie.

--- ComputerGraphicStudies/MainWindow.xaml.cs
+++ ComputerGraphicStudies/MainWindow.xaml.cs
@@ -32,7 +32,7 @@ namespace ComputerGraphicStudies
             InitializeComponent();
         }
 
-        private void CanvasMouseMove(object sender, MouseEventArgs e)
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
         {
             Point pt = e.GetPosition(canvas);
             mousePosition.Content = $"({pt.X:N0}, {pt.Y:N0})";
@@ -57,7 +57,7 @@ namespace ComputerGraphicStudies
             }
         }
 
-        private void CanvasMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             Mouse.Capture(canvas);
             Shape shape;
@@ -84,16 +84,121 @@ namespace ComputerGraphicStudies
                 shape.SetValue(Canvas.TopProperty, start.Y);
             }
 
+            if (newestShape != null)
+            {
+                newestShape.StrokeThickness = 1;
+            }
             newestShape = shape;
             start = e.GetPosition(canvas);
+            shape.MouseLeftButtonDown += Shape_MouseLeftButtonDown;
+            shape.MouseMove += Shape_MouseMove;
             canvas.Children.Add(shape);
             mouseLeftButtonClicked = true;
         }
 
-        private void CanvasMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        private void Canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             mouseLeftButtonClicked = false;
             Mouse.Capture(null);
+        }
+
+        private void Canvas_DragOver(object sender, DragEventArgs e)
+        {
+            Point dropPosition = e.GetPosition(canvas);
+            if (newestShape is Line)
+            {
+                Line line = newestShape as Line;
+                double x2 = dropPosition.X - line.X1;
+                double y2 = dropPosition.Y - line.Y1;
+                line.X1 = dropPosition.X;
+                line.Y1 = dropPosition.Y;
+                line.X2 += x2;
+                line.Y2 += y2;
+            }
+            else
+            {
+                Canvas.SetLeft(newestShape, dropPosition.X);
+                Canvas.SetTop(newestShape, dropPosition.Y);
+            }
+        }
+
+        private void EnableMoving(object sender, RoutedEventArgs e)
+        {
+            canvas.MouseLeftButtonDown -= Canvas_MouseLeftButtonDown;
+            canvas.MouseLeftButtonUp -= Canvas_MouseLeftButtonUp;
+        }
+
+        private void DisableMoving(object sender, RoutedEventArgs e)
+        {
+            canvas.MouseLeftButtonDown += Canvas_MouseLeftButtonDown;
+            canvas.MouseLeftButtonUp += Canvas_MouseLeftButtonUp;
+        }
+
+        private void CreateShape(object sender, RoutedEventArgs e)
+        {
+            //dodac warunek czy inputy sa puste.
+            // Linia nie jest rysowana.
+            Shape shape;
+            double x1 = Double.Parse(x1Input.Text);
+            double y1 = Double.Parse(y1Input.Text);
+            double x2 = Double.Parse(x2Input.Text);
+            double y2 = Double.Parse(y2Input.Text);
+
+            switch (shapeType.SelectedValue)
+            {
+                case "Prostokąt":
+                    shape = new Rectangle();
+                    shape.Name = "Rectangle";
+                    break;
+                case "Elipsa":
+                    shape = new Ellipse();
+                    shape.Name = "Ellipse";
+                    break;
+                default:
+                    shape = new Line();
+                    shape.Name = "Line";
+                    break;
+            }
+
+            shape.Fill = new SolidColorBrush(Colors.Pink);
+            shape.Stroke = new SolidColorBrush(Colors.Red);
+            shape.StrokeThickness = 3;
+            if (!(shape is Line))
+            {
+                shape.SetValue(Canvas.LeftProperty, Double.Parse(x1Input.Text));
+                shape.SetValue(Canvas.TopProperty, Double.Parse(y1Input.Text));
+                shape.Width = x2 - x1;
+                shape.Height = y2 - y1;
+            }
+            else
+            {
+                Line line = shape as Line;
+                line.X1 = x1;
+                line.Y1 = y1;
+                line.X2 = x2;
+                line.Y2 = y2;
+            }
+
+            newestShape = shape;
+            canvas.Children.Add(shape);
+        }
+
+        private void Shape_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (newestShape != null)
+            {
+                newestShape.StrokeThickness = 1;
+            }
+            newestShape = sender as Shape;
+            newestShape.StrokeThickness = 3;
+        }
+
+        private void Shape_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragDrop.DoDragDrop(newestShape, newestShape, DragDropEffects.Move);
+            }
         }
     }
 }
